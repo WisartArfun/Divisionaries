27f036f2b3636dd741a5711f0f56e975ee9fdcf3
WisartArfun <daro.daro15@gmail.com>
2019-08-29 23:22:01 +0200

    first protocol success

---
+++ scripts/State.js
@@ -0,0 +1,43 @@
+import { Map } from './Renderer.js';
+
+class State {
+    constructor(x_size, y_size, canvas) { //, map) { // [DANGER] create map or pass map???
+        this.state = {};
+        this.state.x_fields = x_size;
+        this.state.y_fields = y_size;
+
+        let col = new Array(this.state.x_fields);
+        col.fill({ type: 'fog' });
+        this.state.fields = new Array(this.state.y_fields);
+        this.state.fields.fill(col);
+
+        this.map = new Map(canvas, this.state);
+    }
+
+    update(x, y, new_state) {
+        this.map.update_single_state(x, y, new_state);
+    }
+}
+
+/////////
+// DONE OUTSIDE
+/////////
+
+// let canvas = document.getElementById('game-canvas');
+// let state_interpreter = new State(10, 10, canvas);
+
+// let kings = [
+//     [1, 2],
+//     [5, 8],
+//     [8, 9],
+//     [8, 1]
+// ];
+// for (let i in kings) {
+//     let k = kings[i];
+//     let x = k[0];
+//     let y = k[1];
+//     state_interpreter.update({ x: x, y: y, state: { type: 'king' } });
+// }
+
+const _State = State;
+export { _State as State };
