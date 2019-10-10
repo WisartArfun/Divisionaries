ea14d6fc24f5d6f9ec83d4ea012e5be598c0d157
WisartArfun <daro.daro15@gmail.com>
2019-10-10 11:26:43 +0200

    implementing div game

--- src/div_game/mod.rs
+++ src/div_game/mod.rs
@@ -40,29 +40,41 @@ impl Bucket for DivGameBucket {
         let content = str::from_utf8(&msg).unwrap(); // PROB: error handling
         if let Ok(api_request) = serde_json::from_str::<APIRequest>(content) {
             match api_request {
-                APIRequest::JoinDivGameNormal => {
-                    log::info!("client joined a normal div game");
-                    client.lock().unwrap().send(serde_json::to_vec(&APIResponse::JoinGame("some_id".to_string())).unwrap()); // PROB: error handling // QUES: efficiency?
-                    let id = client.lock().unwrap().get_id(); // QUES: two times lock bad?
-                    self.connection_handler.lock().unwrap().disconnect_client(id);
-                    log::debug!("client left ApiBucket");
+                DivGameLobbyRequest(lobby_request) => {
+                    match lobby_request {
+                        DivGameLobbyRequest::Ready => {
+                            log::debug!("client is ready");
+                            
+                        },
+                        _ => {
+                            log::warn!("invalid APIRequest send to APIServer");
+                            client.lock().unwrap().send(serde_json::to_vec(&APIResponse::InvalidRequest).unwrap());
+                        },
+                    }
                 },
-                APIRequest::JoinDivGameDirect(id) => {
-                    log::info!("client joined a normal div game direct");
-                    client.lock().unwrap().send(serde_json::to_vec(&APIResponse::JoinGame(id)).unwrap()); // PROB: error handling // QUES: efficiency?
-                    let id = client.lock().unwrap().get_id(); // QUES: two times lock bad?
-                    self.connection_handler.lock().unwrap().disconnect_client(id);
-                    log::debug!("client left ApiBucket");
-                },
-                APIRequest::GetOpenLobbies => {
-                    let lobbies = self.bucket_manager.lock().unwrap().get_open_lobbies();
-                    client.lock().unwrap().send(serde_json::to_vec(&APIResponse::OpenLobbies(lobbies)).unwrap())
-                },
-                APIRequest::GetRunningGames => {
-                    log::debug!("client asked for open lobbies");
-                    let games = self.bucket_manager.lock().unwrap().get_running_games();
-                    client.lock().unwrap().send(serde_json::to_vec(&APIResponse::RunningGames(games)).unwrap())
-                },
+                // APIRequest::JoinDivGameNormal => {
+                //     log::info!("client joined a normal div game");
+                //     client.lock().unwrap().send(serde_json::to_vec(&APIResponse::JoinGame("some_id".to_string())).unwrap()); // PROB: error handling // QUES: efficiency?
+                //     let id = client.lock().unwrap().get_id(); // QUES: two times lock bad?
+                //     self.connection_handler.lock().unwrap().disconnect_client(id);
+                //     log::debug!("client left ApiBucket");
+                // },
+                // APIRequest::JoinDivGameDirect(id) => {
+                //     log::info!("client joined a normal div game direct");
+                //     client.lock().unwrap().send(serde_json::to_vec(&APIResponse::JoinGame(id)).unwrap()); // PROB: error handling // QUES: efficiency?
+                //     let id = client.lock().unwrap().get_id(); // QUES: two times lock bad?
+                //     self.connection_handler.lock().unwrap().disconnect_client(id);
+                //     log::debug!("client left ApiBucket");
+                // },
+                // APIRequest::GetOpenLobbies => {
+                //     let lobbies = self.bucket_manager.lock().unwrap().get_open_lobbies();
+                //     client.lock().unwrap().send(serde_json::to_vec(&APIResponse::OpenLobbies(lobbies)).unwrap())
+                // },
+                // APIRequest::GetRunningGames => {
+                //     log::debug!("client asked for open lobbies");
+                //     let games = self.bucket_manager.lock().unwrap().get_running_games();
+                //     client.lock().unwrap().send(serde_json::to_vec(&APIResponse::RunningGames(games)).unwrap())
+                // },
                 _ => {
                     log::warn!("invalid APIRequest send to APIServer");
                     client.lock().unwrap().send(serde_json::to_vec(&APIResponse::InvalidRequest).unwrap());
