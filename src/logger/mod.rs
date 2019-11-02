1d6b6a27424ac35691ee2cd4b4028292cf4f4b22
WisartArfun <daro.daro15@gmail.com>
2019-11-02 14:07:27 +0100

    log4rs example

--- src/logger/mod.rs
+++ src/logger/mod.rs
@@ -24,6 +24,40 @@ use log4rs;
 /// logger::init("tests/log4rs.yaml");
 /// log::debug!("This is a log message on the debug level.");
 /// ```
+/// 
+/// Here is an example config file,
+/// more details can be found in the documentation for `log4rs`.
+/// 
+/// ```yaml
+/// refresh_rate: 30 seconds
+/// appenders:
+// /  stdout_detailed:
+///     kind: console
+///     encoder:
+///       pattern: "{f}{n}{M} - l: {L}{n}{d(%Y-%m-%d %H:%M:%S %Z)(utc)} [{h({l})}] - {m}{n}"
+///   stdout:
+///     kind: console
+///     encoder:
+///       pattern: "{d(%H:%M:%S %Z)(utc)} [{h({l})}] - {m}{n}"
+///   file:
+///     kind: file
+///     path: "logs/log.log"
+///     encoder:
+///       pattern: "{f}{n}{M} - l: {L}{n}{d(%Y-%m-%d %H:%M:%S %Z)(utc)} [{h({l})}] - {m}{n}"
+/// root:
+///   level: debug
+///   appenders:
+///     - stdout
+///     - file
+/// loggers:
+///   app::backend::db:
+///     level: debug
+///   app::requests:
+///     level: debug
+///     appenders:
+///       - file
+///     additive: false
+/// ```
 pub fn init(src: &str) {
     log4rs::init_file(src, Default::default()).unwrap(); // this is global
     log::info!("log4rs initialized from config file at src: {}", src);
