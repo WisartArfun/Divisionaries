f74c80cb15c29ace4aab7adacc355d970381d4f2
WisartArfun <daro.daro15@gmail.com>
2019-11-15 20:23:12 +0100

    add by closure

--- src/web_socket/mod.rs
+++ src/web_socket/mod.rs
@@ -1,8 +1,61 @@
 //! handles websocket connections and servers
 
+pub use connection::WSConnection;
+pub use server::WebSocketServer;
+
 /// handles a connection
 mod connection {
+    use std::net::TcpStream;
+    use std::sync::{Arc, Mutex};
 
+    use tungstenite;
+
+    pub struct WSConnection {
+        ws_conn: Arc<Mutex<tungstenite::protocol::WebSocket<TcpStream>>>,
+    }
+
+    impl WSConnection {
+        pub fn new(ws_conn: tungstenite::protocol::WebSocket<TcpStream>) -> Self {
+            log::info!("creating new WSConnection");
+            Self {
+                ws_conn: Arc::new(Mutex::new(ws_conn))
+            }
+        }
+
+        pub fn close(&mut self) { // WARN: do this with a trait
+            log::info!("closing WSConnection");
+            self.ws_conn.lock().unwrap().close(None).unwrap(); // WARN: unsafe unwrap
+        }
+
+        pub fn send(&mut self, message: Vec<u8>) -> Result<(), tungstenite::error::Error> { // QUES: return other error type
+            log::debug!("sending message over WSConnection");
+            self.ws_conn.lock().unwrap().write_message(tungstenite::Message::Binary(message))?;
+            Ok(())
+        }
+
+        pub fn try_recv(&mut self) -> Result<Option<Vec<u8>>, tungstenite::error::Error> {
+            match self.ws_conn.lock().unwrap().read_message() {
+                Ok(msg) => {
+                    log::debug!("received message from WSConnection.websocket");
+                    if msg.is_binary() || msg.is_text() {
+                        return Ok(Some(msg.into_data()));
+                    }
+                },
+                // QUEST: correct error handling
+                Err(e) => {
+                    if let tungstenite::error::Error::Io(err) = &e {
+                        if err.kind() == std::io::ErrorKind::WouldBlock {
+                            return Ok(None);
+                        };
+                    }
+                    log::warn!("an error occured while receiving a message from WSConnection: {}", &e);
+                    return Err(e);
+                }
+            };
+
+            Ok(None)
+        }
+    }
 }
 
 /// creates a `WebSocketServer` that can accept connections
@@ -14,7 +67,7 @@ pub mod server {
 
     use tungstenite;
 
-    pub struct Test;
+    use super::connection::WSConnection;
 
     /// a simple `WebSocketServer` that will handle all incoming connections with a closure
     /// 
@@ -60,7 +113,7 @@ pub mod server {
         /// 
         /// # Type Parameters
         /// 
-        /// * F: `FnMut(Test) -> () + Send + 'static` - type for the callback
+        /// * F: `FnMut(WSConnection) -> () + Send + 'static` - type for the callback
         /// 
         /// # Arguments
         /// 
@@ -69,7 +122,7 @@ pub mod server {
         /// # Returns
         /// 
         /// * handle: `Option<thread::JoinHandle<std::io::Result<()>>>` - returns a handle if the server was not already running
-        pub fn start<F: FnMut(Test) -> () + Send + 'static>(&mut self, mut callback: F) -> Option<thread::JoinHandle<std::io::Result<()>>> {
+        pub fn start<F: FnMut(WSConnection) -> () + Send + 'static>(&mut self, mut callback: F) -> Option<thread::JoinHandle<std::io::Result<()>>> {
             if self.running {
                 log::warn!("trying to start WebSocketServer, although it is already running");
                 return None;
@@ -90,9 +143,9 @@ pub mod server {
                     thread::sleep(Duration::from_millis(5));
                     
                     match tungstenite::server::accept(stream) {
-                        Ok(_socket) => {
+                        Ok(socket) => {
                             log::debug!("calling callbock from WebSocketServer");
-                            callback(Test{});
+                            callback(WSConnection::new(socket));
                         },
                         Err(e) => log::warn!("an error occured while accepting an incoming connection: {}", e), // WARN: server will continue even if an error occures
                     }
