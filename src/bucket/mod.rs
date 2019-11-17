a080047f251a2a7aa1f5590da28f4efaaf52bc0b
WisartArfun <daro.daro15@gmail.com>
2019-11-17 19:56:10 +0100

    bucket types base

---
+++ src/bucket/mod.rs
@@ -0,0 +1,5 @@
+//! holds all data types needed to create a custom `Bucket`
+
+mod types;
+
+pub use types::{BucketMessage, BucketClient, Bucket};
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,4 @@ pub mod file_manager;
 pub mod web_server;
 pub mod logger;
 pub mod web_socket;
+pub mod bucket;
