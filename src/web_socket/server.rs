3172791af04d6668f3a439ce4c939e6456f3265f
WisartArfun <daro.daro15@gmail.com>
2019-11-27 16:52:03 +0100

    small additions

--- src/web_socket/server.rs
+++ src/web_socket/server.rs
@@ -11,33 +11,55 @@ use super::WSConnection; // QUES: nice way to do it or make mod public and get i
 
 /// a simple `WebSocketServer` that will handle all incoming connections with a closure
 /// 
-/// # Type Parameters
-/// 
-/// * `'a` - the lifetime for ip and port
-/// 
 /// # Variables
 /// 
-/// * ip: `&'a str` - the ip of the `WebSocketServer`
-/// * port: `&'a str` - the port of the `WebScoketServer`
+/// * ip: `String` - the ip of the `WebSocketServer`
+/// * port: `String` - the port of the `WebScoketServer`
 /// * running: `bool` - whether the `WebSocketServer` is running or not
-pub struct WebSocketServer<'a> {
-    ip: &'a str,
-    port: &'a str,
+/// 
+/// # Examples
+/// 
+/// Creates a WebSocketServer and adds all clients that connect to a `Arc<Mutex<Vec<WSConnection>>>`.
+/// 
+/// ```rust
+/// use std::sync::{Arc, Mutex};
+/// 
+/// use bucketer::web_socket::WebSocketServer;
+/// 
+/// let mut web_socket = WebSocketServer::new("127.0.0.1".to_string(), "8000".to_string());
+/// 
+/// let clients = Arc::new(Mutex::new(Vec::new()));
+/// let clients_cp = clients.clone();
+/// 
+/// web_socket.start(move |inst| -> () {
+///    clients_cp.lock().unwrap().push(inst);
+/// });
+/// ```
+pub struct WebSocketServer {
+    ip: String,
+    port: String,
     running: bool,
 }
 
-impl<'a> WebSocketServer<'a> {
+// QUES: too much effort???
+// pub struct WebSocketServer<'a> {
+//     ip: &'a str,
+//     port: &'a str,
+//     running: bool,
+// }
+
+impl WebSocketServer {
     /// creates a new `WebSocketServer` instance
     /// 
     /// # Arguments
     /// 
-    /// * ip: `&'a str` - the ip of the `WebSocketServer`
-    /// * port: `&'a str` - the port of the `WebSocketServer`
+    /// * ip: `String` - the ip of the `WebSocketServer`
+    /// * port: `String` - the port of the `WebSocketServer`
     /// 
     /// # Returns
     /// 
     /// ws_instance: `WebSocketServer` - an instance of a `WebScoketServer`
-    pub fn new(ip: &'a str, port: &'a str) -> Self {
+    pub fn new(ip: String, port: String) -> Self {
         Self {
             ip,
             port,
@@ -93,5 +115,28 @@ impl<'a> WebSocketServer<'a> {
 
             Ok(())
         }))
+    }
+
+    /// stops the WebSocket instance
+    pub fn stop(&mut self) {
+        unimplemented!();
+    }
+
+    /// returns the ip of the WebScocket instance
+    /// 
+    /// # Returns
+    /// 
+    /// * ip: `&str` - the ip of the WebScoket
+    pub fn get_ip(&self) -> &str {
+        &self.ip
+    }
+
+    /// returns the ip of the WebScocket instance
+    /// 
+    /// # Returns
+    /// 
+    /// * port: `&str` - the port of the WebScoket
+    pub fn get_port(&self) -> &str {
+        &self.port
     }
 }
