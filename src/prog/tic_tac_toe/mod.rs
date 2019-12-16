ef3c1a1f8579aee16d35a48eeb56b3fb458c84cf
WisartArfun <daro.daro15@gmail.com>
2019-12-16 11:04:37 +0100

    base running

--- src/prog/tic_tac_toe/mod.rs
+++ src/prog/tic_tac_toe/mod.rs
@@ -71,9 +71,62 @@ impl Bucket for TicTacToe {
                     self.state[y as usize][x as usize] = Some(self.turn);
                     let state = serde_json::to_vec(&Response::State(self.state.clone())).expect("serializing failed");
                     self.connection_handler.lock().unwrap().broadcast(state).unwrap();
-                    self.turn = !self.turn;
+                    
+                    let mut winner = None;
+                    for i in 0..2 {
+                        let field = match self.state[i][0] {
+                            Some(field) => field,
+                            _ => continue,
+                        };
+                        match self.state[i][1] {
+                            Some(f) => if f != field {continue;},
+                            _ => continue,
+                        }
+                        match self.state[i][2] {
+                            Some(f) => if f == field {winner = Some(self.turn)},
+                            _ => continue,
+                        }
+                    }
 
-                    // TODO: add winning and losing
+                    for i in 0..2 {
+                        let field = match self.state[0][i] {
+                            Some(field) => field,
+                            _ => continue,
+                        };
+                        match self.state[1][i] {
+                            Some(f) => if f != field {continue;},
+                            _ => continue,
+                        }
+                        match self.state[2][i] {
+                            Some(f) => if f == field {winner = Some(self.turn)},
+                            _ => continue,
+                        }
+                    }
+
+                    let part = self.state[0][0];
+                    if part.is_some() {
+                        if self.state[0][0] == self.state[1][1] && self.state[1][1] == self.state[2][2] {
+                            winner = Some(self.turn);
+                        }
+                    }
+                    
+                    let part = self.state[2][0];
+                    if part.is_some() {
+                        if self.state[2][0] == self.state[1][1] && self.state[1][1] == self.state[0][2] {
+                            winner = Some(self.turn);
+                        }
+                    }
+
+                    if let Some(winner) = winner {
+                        let wid = self.players[if winner {0} else {1}];
+                        let lid = self.players[if winner {1} else {0}];
+                        let w_message = serde_json::to_vec(&Response::Win).expect("serializing failed");
+                        let l_message = serde_json::to_vec(&Response::Defeat).expect("serializing failed");
+                        self.connection_handler.lock().unwrap().connections[&wid].lock().unwrap().send(w_message).expect("sending failed");
+                        self.connection_handler.lock().unwrap().connections[&lid].lock().unwrap().send(l_message).expect("sending failed");
+                    }
+
+                    self.turn = !self.turn;
                 }
             }
         } else {
